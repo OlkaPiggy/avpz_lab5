5fec02d1f6e9943c5bd0e65f889c676bcdb1d827
olha.sopotnytska <olha.sopotnytska.pz.2020@lpnu.ua>
2022-11-29 03:13:25 +0200

    done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,11 +5,11 @@ import css from './Header.module.css';
 const Header = () => {
     return (
         <header className={css.links}>
-            <NavLink to={'source'}>Джерела</NavLink>
-            <NavLink to={'identification'}>Ідентифікація</NavLink>
-            <NavLink to={'appearance'}>Поява</NavLink>
-            <NavLink to={'loss'}>Втрати</NavLink>
-            <NavLink to={'planning'}>Планування</NavLink>
+            <NavLink to={'source'}>Джерела ризиків</NavLink>
+            <NavLink to={'identification'}>Ідентифікація ризиків</NavLink>
+            <NavLink to={'appearance'}>Аналіз ризиків</NavLink>
+            <NavLink to={'loss'}>Обчислення втрат</NavLink>
+            <NavLink to={'planning'}>Планування ризиків</NavLink>
         </header>
     );
 };
